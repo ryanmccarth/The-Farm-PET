24413c0cd1450b045665a7725a9b441b0572f778
Stephan Lensky <stephanl.public@gmail.com>
2020-03-24 15:04:48 -0400

    initial docker bootstrap with separate client/api packages

--- pet-client/src/App.js
+++ pet-client/src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import mysql from 'mysql'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  console.log('Hello from Docker!');
+  var con = mysql.createConnection({
+      host: "db",
+      user: "bessy",
+      password: "the-cow"
+  });
+  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+  async function waitForConnection() {
+      console.log("waiting...")
+      await sleep(10000)
+      console.log('Testing MySQL connection...');
+      con.connect(async function(err) {
+          if (err) throw err;
+          console.log("Connected!");
+      });
+  }
+  waitForConnection()
+
   return (
     <div className="App">
       <header className="App-header">
