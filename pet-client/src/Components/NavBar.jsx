0e995af4d198ddf9554355f4dc38b8877b59d78f
ztwilliams26 <zwilliams6886@gmail.com>
2020-03-27 21:16:20 -0400

    Fixed active page style in nav bar

--- pet-client/src/Components/NavBar.jsx
+++ pet-client/src/Components/NavBar.jsx
@@ -6,8 +6,6 @@ class NavBar extends Component {
   }
 
   render() {
-    //TODO: Add different styling depending on the current page (found in this.props.page)
-
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <span className="navbar-brand">PET</span>
@@ -26,40 +24,66 @@ class NavBar extends Component {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
+            <li
+              className={
+                "nav-item" + (this.props.page === "login" && " active")
+              }
+            >
               <a
                 className="nav-link"
                 href="#top"
                 onClick={() => this.pageHandler("login")}
               >
-                Login <span className="sr-only">(current)</span>
+                Login
+                {this.props.page === "login" && (
+                  <span className="sr-only">(current)</span>
+                )}
               </a>
             </li>
-            <li className="nav-item">
+            <li
+              className={"nav-item" + (this.props.page === "read" && " active")}
+            >
               <a
                 className="nav-link"
                 href="#top"
                 onClick={() => this.pageHandler("read")}
               >
                 Read
+                {this.props.page === "read" && (
+                  <span className="sr-only">(current)</span>
+                )}
               </a>
             </li>
-            <li className="nav-item">
+            <li
+              className={
+                "nav-item" + (this.props.page === "request" && " active")
+              }
+            >
               <a
                 className="nav-link"
                 href="#top"
                 onClick={() => this.pageHandler("request")}
               >
                 Request
+                {this.props.page === "request" && (
+                  <span className="sr-only">(current)</span>
+                )}
               </a>
             </li>
-            <li className="nav-item">
+            <li
+              className={
+                "nav-item" + (this.props.page === "write" && " active")
+              }
+            >
               <a
                 className="nav-link"
                 href="#top"
                 onClick={() => this.pageHandler("write")}
               >
                 Write
+                {this.props.page === "write" && (
+                  <span className="sr-only">(current)</span>
+                )}
               </a>
             </li>
             <li className="nav-item">
