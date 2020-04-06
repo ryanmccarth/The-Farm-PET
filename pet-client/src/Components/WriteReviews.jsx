15a52bd79c59221486cff81ea6e35644cce3396a
Stephan Lensky <stephanl.public@gmail.com>
2020-04-05 22:03:14 -0400

    Finalize mockup for write reviews

--- pet-client/src/Components/WriteReviews.jsx
+++ pet-client/src/Components/WriteReviews.jsx
@@ -2,39 +2,46 @@ import React, { Component } from "react";
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
+
+import './temp/WriteContent.css'
 
 class WriteReviews extends Component {
   state = {};
+  // we should implement something to get this from the database (depending on prev screen)
+  revieweename = "Joe Smith";
+
+  backButton() {
+    console.log("back button pressed");
+  }
+
+  async submitReview(e) {
+    e.preventDefault();
+    console.log("success");
+  }
+
   render() {
     return <React.Fragment>
-      <Container className="write-container d-flex">
-        <Container className="row justify-content-center align-self-center">
-          <Form id="write-review-form" onSubmit={submitReview}>
-            <h4>You are writing a review for {revieweename}</h4>
-
-            <Form.Group controlId="write-review-textarea">
-              <Form.Control as="textarea" rows="6" placeholder = "Write review here..." />
-            </Form.Group>
-
-            <Row className="justify-content-between">
-              <Button
-              variant = "primary"
-              onClick = {console.log("back button pressed")}>
-                Go back
-              </Button>
-
-              <Button
-              variant = "primary"
-              type = "submit">
-                Submit
-              </Button>
-            </Row>
-            <Row>
-              <div id = "submitresult"></div>
-            </Row>
-          </Form>
-        </Container>
+      <Container id="write-container">
+        <h4>Write a review for {this.revieweename}</h4>
+        <Form id="write-review-form" onSubmit={this.submitReview}>
+          <Form.Group controlId="write-review-textarea">
+            <Form.Control as="textarea" rows="6" placeholder = "Write review here..." required />
+          </Form.Group>
+          <div id="write-btn-back">
+            <Button
+            variant = "primary"
+            onClick = {this.backButton}>
+              Go back
+            </Button>
+          </div>
+          <div id="write-btn-submit" className="text-right">
+            <Button
+            variant = "primary"
+            type = "submit">
+              Submit
+            </Button>
+          </div>
+        </Form>
       </Container>
     </React.Fragment>;
   }
