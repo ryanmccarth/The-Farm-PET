41d8c26464f59bdf58af09ae96eb37867f179083
Stephan Lensky <stephanl.public@gmail.com>
2020-04-17 16:39:16 -0400

    Add stubs for requests router

--- pet-api/app.js
+++ pet-api/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var jsonErrorHandler = require('express-json-error-handler');
 
 var authRouter = require('./routes/auth');
+var requestsRouter = require('./routes/requests');
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/auth', authRouter);
+app.use('/api/requests', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
