37173416f77fd246bf8d15e01e80946a9c916a88
unknown <trif4o99@gmail.com>
2020-04-19 20:19:14 -0400

    Added methods insertReview and editReview in db.js

--- pet-api/db.js
+++ pet-api/db.js
@@ -48,6 +48,50 @@ class PetDB {
             });
         });
     }
+    
+    // Purpose: to return a Promise that adds a review row to the reviews table upon resolving
+    // Input:   reviewerId - integer representing the writer's Id (userId, not companyUserId)
+    //          revieweeId - integer representing the receiver's Id (userId, not companyUserId)
+    //          text       - string to be written as the reviewText
+    //          dateTime   - date and time of submission in the format "YYYY-MM-DD HH:MI:SS" (important)
+    //          isDraft    - boolean representing whether this is a draft or final
+    // Returns: Promise object that inserts a review row with given parameters upon resolving  
+    insertReview(reviewerId, revieweeId, text, datetime, isDraft) {
+        if(!this.connected) return;                 // check connection
+        
+        const c = this.c;
+        draftStatus = isDraft ? "TRUE" : "FALSE";   // make answer uppercase for mySQL to handle
+
+        // TODO: should we check if users exist before inserting?? if not, disregard
+        return new Promise((resolve) => {
+            c.query(`INSERT INTO reviews (writtenBy, writtenFor, reviewText, lastUpdated, isDraft) VALUES (${reviewerId}, ${revieweeId}, '${text}', '${datetime}', ${draftStatus})`, function (error, results, fields) {
+                if(error) throw error;
+                
+                resolve(null);
+            });
+        });
+    }
+    
+    // Purpose: to return a Promise that updates the text, datetime and draft status of a review upon resolving
+    // Input:   reviewId - integer representing the Id of the review that will be edited
+    //          text     - string to be written as reviewText
+    //          dateTime - date and time of edit in the format "YYYY-MM-DD HH:MI:SS" (important)
+    //          isDraft  - boolean representing whether this is a draft or final
+    // Returns: Promise object that replaces the reviewText, lastUpdated and isDraft fields in the review 
+    //          referenced by reviewId upon resolving
+    editReview(reviewId, text, datetime, isDraft) {
+        if(!this.connected) return;
+        const c = this.c;
+        const draftStatus = isDraft ? "TRUE" : "FALSE";
+        
+        return new Promise((resolve) => {
+            c.query(`UPDATE reviews SET reviewText = '${text}', lastUpdated = '${datetime}', isDraft = ${draftStatus} WHERE reviewId = ${reviewId}`, function(error, results, fields){
+                if(error) throw error;
+
+                resolve(null);
+            });
+        });
+    }
 }
 
 module.exports = new PetDB();
