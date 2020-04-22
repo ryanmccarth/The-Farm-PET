9ea378d4ace94b771477e58fbdaa1ad2a8445f4f
ztwilliams26 <zwilliams6886@gmail.com>
2020-04-22 14:00:57 -0400

    More work on test container

--- pet-test/login.test.js
+++ pet-test/login.test.js
@@ -1,13 +1,13 @@
 test("Login correct credentials", async () => {
-  let res = await fetch("/api/auth", {
+  let res = await fetch("http://api:3001/api/auth", {
     method: "POST",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      username: username,
-      password: password,
+      username: "Reed_Fischer@bluellamaconsultingco.com",
+      password: "fischerre",
     }),
   });
   expect(res).toContain("token");
