cc3fd12a4de4bd1b399bce012968c90f2b431a4a
ztwilliams26 <zwilliams6886@gmail.com>
2020-04-29 23:53:54 -0400

    Fixed navbar dropdown button

--- pet-client/src/Components/NavBar.jsx
+++ pet-client/src/Components/NavBar.jsx
@@ -3,6 +3,14 @@ import ConditionalTooltip from "./ConditionalTooltip";
 import session from "../session";
 
 class NavBar extends Component {
+  state = {
+    navCollapsed: true,
+  };
+
+  _onToggleNav = () => {
+    this.setState({ navCollapsed: !this.state.navCollapsed });
+  };
+
   pageHandler(page) {
     this.props.updateContent(page);
   }
@@ -13,6 +21,8 @@ class NavBar extends Component {
   }
 
   render() {
+    const { navCollapsed } = this.state;
+
     const disabledButtonTooltip = "Log in to access this page";
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -26,18 +36,27 @@ class NavBar extends Component {
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
+          onClick={this._onToggleNav}
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div
+          className={(navCollapsed ? "collapse" : "") + " navbar-collapse"}
+          id="navbarSupportedContent"
+        >
           <ul className="navbar-nav mr-auto">
             <li
               className={"nav-item" + (this.props.page === "read" && " active")}
             >
-              <ConditionalTooltip tooltip={disabledButtonTooltip} show={!session.exists()}>
+              <ConditionalTooltip
+                tooltip={disabledButtonTooltip}
+                show={!session.exists()}
+              >
                 <a
-                  className={"nav-link" + (!session.exists() ? " disabled" : "")}
+                  className={
+                    "nav-link" + (!session.exists() ? " disabled" : "")
+                  }
                   href="#top"
                   onClick={() => this.pageHandler("read")}
                 >
@@ -53,9 +72,14 @@ class NavBar extends Component {
                 "nav-item" + (this.props.page === "request" && " active")
               }
             >
-              <ConditionalTooltip tooltip={disabledButtonTooltip} show={!session.exists()}>
+              <ConditionalTooltip
+                tooltip={disabledButtonTooltip}
+                show={!session.exists()}
+              >
                 <a
-                  className={"nav-link" + (!session.exists() ? " disabled" : "")}
+                  className={
+                    "nav-link" + (!session.exists() ? " disabled" : "")
+                  }
                   href="#top"
                   onClick={() => this.pageHandler("request")}
                 >
@@ -71,9 +95,14 @@ class NavBar extends Component {
                 "nav-item" + (this.props.page === "write" && " active")
               }
             >
-              <ConditionalTooltip tooltip={disabledButtonTooltip} show={!session.exists()}>
+              <ConditionalTooltip
+                tooltip={disabledButtonTooltip}
+                show={!session.exists()}
+              >
                 <a
-                  className={"nav-link" + (!session.exists() ? " disabled" : "")}
+                  className={
+                    "nav-link" + (!session.exists() ? " disabled" : "")
+                  }
                   href="#top"
                   onClick={() => this.pageHandler("write")}
                 >
@@ -86,38 +115,39 @@ class NavBar extends Component {
             </li>
           </ul>
           <ul className="navbar-nav navbar-right">
-            {session.exists()
-            ? /* forgive me for putting the styles here but it's way easier than creating a CSS file */
+            {session.exists() ? (
+              /* forgive me for putting the styles here but it's way easier than creating a CSS file */
               <>
-              <li className="nav-item" style={{
-                display: "block",
-                paddingTop: "0.5rem",
-                paddingBottom: "0.5rem",
-                color: "rgba(0,0,0,.5)",
-                marginRight: "0.5rem"
-              }}>
-                Hello, {session.firstName}
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="top"
-                  onClick={() => this.sessionLogout()}
+                <li
+                  className="nav-item"
+                  style={{
+                    display: "block",
+                    paddingTop: "0.5rem",
+                    paddingBottom: "0.5rem",
+                    color: "rgba(0,0,0,.5)",
+                    marginRight: "0.5rem",
+                  }}
                 >
-                  Log Out
-                </a>
-              </li>
+                  Hello, {session.firstName}
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link"
+                    href="top"
+                    onClick={() => this.sessionLogout()}
+                  >
+                    Log Out
+                  </a>
+                </li>
               </>
-            : <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href=""
-                >
+            ) : (
+              <li className="nav-item">
+                <a className="nav-link" href="#">
                   Log in
                 </a>
-              </li>}
+              </li>
+            )}
           </ul>
-
         </div>
       </nav>
     );
